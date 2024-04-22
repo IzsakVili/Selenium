3db92e1cf648c000cc77e4a1aa3fddb952f03a2a
izsak.vilmos.daniel <izsak.vilmos.daniel@BOT209-016>
2024-04-22 08:34:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var options = new EdgeOptions() { ImplicitWaitTimeout = TimeSpan.FromSeconds(5) };
+            var driver = new EdgeDriver(options);
+            driver.Url = "https://google.com";
+            driver.FindElement(By.Id("W0wltc")).Click();
+            driver.FindElements(By.Name("btnI")).Last().Click();
+            driver.ExecuteAsyncScript    
+                
+                
+                
+                
+                }
+    }
+}
